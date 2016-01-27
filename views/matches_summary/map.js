893bb5cf1dcdd98ba6596f212b0314b5a726da60
Santiago Saavedra <santiagosaavedra@gmail.com>
2016-01-28 00:59:09 +0100

    Add match_summary

--- views/matches_summary/map.js
+++ views/matches_summary/map.js
@@ -4,7 +4,7 @@
 function(doc) {
   if(typeof doc.type !== "undefined") {
     if(doc.type == 'match') {
-      emit([doc._id, 0], { "_id": doc._id });
+      emit([doc._id, 0], doc);
     }
     else if(doc.type == 'event') {
       emit([doc.match, doc.team, doc.eventType, doc.date], { "_id": doc._id });
--- views/matches_summary/reduce.js
+++ views/matches_summary/reduce.js
@@ -1,4 +1,7 @@
 
 function (key, docs, rereduce) {
+  if(key[0][0][1] == 0 && docs.length == 1)
+    return docs[0];
+
   return docs.length;
 }
